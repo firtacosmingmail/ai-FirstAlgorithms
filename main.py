c750fed4370f44c39a5dd7000041632c3f6dbc4e
Firta Cosmin <>
2023-06-07 16:46:47 +0300

    K Means Clustering algorithm

---
+++ main.py
@@ -0,0 +1,8 @@
+import linear_algo
+import knn_algo
+import SVM_supportVectorMachines_algo
+import KMeansClustering
+
+
+# SVM_supportVectorMachines_algo.svm_algorithm()
+KMeansClustering.KMeansClusteringAlgo()
